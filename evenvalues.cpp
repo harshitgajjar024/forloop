836e4e9c6e9ad04755cff4a0d9fe9319ac392d81
harshitgajjar024 <146190333+harshitgajjar024@users.noreply.github.com>
2024-08-01 12:19:03 +0530

    Add files via upload

---
+++ evenvalues.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for (int i = 0; i < 101; i=i+2)
+    {
+        cout<<"number is "<<i<<endl;
+    }
+    return 0;
+}
---
+++ loopsir5times.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for (int i = 0; i < 6; i++)
+    {
+        cout<<"sit"<<endl;
+    }
+    return 0;
+}
